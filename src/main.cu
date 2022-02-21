5becb9a21017402a1da97f9ed7a1b5e7f502e9e2
Axel Nyström Svensson <axel.nystrom-svensson@hotmail.com>
2022-02-21 16:01:52 +0100

    gradient output

--- src/main.cu
+++ src/main.cu
@@ -2,64 +2,64 @@
 #include <math.h>
 #include <stdio.h>
 
-#define IMAGE_WIDTH 100
-#define IMAGE_HEIGHT 100
-
+#define IMAGE_WIDTH 256
+#define IMAGE_HEIGHT 256
+#define PIXEL_STRIDE 3
+#define PIXEL_TYPE unsigned char
 
 __global__
-void add(int n, float *x, float *y){
-  for (int i = 0; i < n; i++)
-    y[i] = x[i] + y[i];
+void render(float *image){
+  int i = threadIdx.x + blockIdx.x * blockDim.x;
+  int j = threadIdx.y + blockIdx.y * blockDim.y;
+  if((i >= IMAGE_WIDTH) || (j >= IMAGE_HEIGHT)) return;
+  int pixel_index = j*IMAGE_WIDTH*3 + i*3;
+  image[pixel_index + 0] = float(i) / IMAGE_WIDTH;
+  image[pixel_index + 1] = float(j) / IMAGE_HEIGHT;
+  image[pixel_index + 2] = 0.2;
 }
 
-void fileio(void){
-  const int dimx = IMAGE_WIDTH, dimy = IMAGE_HEIGHT;
-  int i, j;
-  FILE *fp = fopen("first.ppm", "wb");
-  fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
-  for (j = 0; j < dimy; ++j){
-    for (i = 0; i < dimx; ++i){
-      static unsigned char color[3];
-      color[0] = i % 256;  /* red */
-      color[1] = j % 256;  /* green */
-      color[2] = (i * j) % 256;  /* blue */
-      fwrite(color, 1, 3, fp);
+int write_image_buffer_to_disk(float *ptr_img, const char *fileName){
+  // Write image to disk
+  FILE *fp = fopen(fileName, "w");
+  fprintf(fp, "P3\n%d %d\n255\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+  for (int j = IMAGE_HEIGHT-1; j >= 0; j--) {
+    for (int i = 0; i < IMAGE_WIDTH; i++) {
+      size_t pixel_index = j*PIXEL_STRIDE*IMAGE_WIDTH + i*PIXEL_STRIDE;
+      float r = ptr_img[pixel_index + 0];
+      float g = ptr_img[pixel_index + 1];
+      float b = ptr_img[pixel_index + 2];
+      int ir = int(255.99*r);
+      int ig = int(255.99*g);
+      int ib = int(255.99*b);
+      fprintf(fp, "%d %d %d\n", ir, ig, ib);
     }
   }
+
   fclose(fp);
+  return 0;
 }
 
 int main(void){
-  fileio();
+  const int img_pixels = IMAGE_HEIGHT * IMAGE_WIDTH;
+  float *ptr_img;
+  int img_size = img_pixels*sizeof(float)*PIXEL_STRIDE;
 
-  // int N = 1<<20;
-  // float *x, *y;
+  int tx = 8;
+  int ty = 8;
 
-  // // Allocate Unified Memory – accessible from CPU or GPU
-  // cudaMallocManaged(&x, N*sizeof(float));
-  // cudaMallocManaged(&y, N*sizeof(float));
+  dim3 blocks(IMAGE_WIDTH/tx+1,IMAGE_HEIGHT/ty+1);
+  dim3 threads(tx,ty);
 
-  // // initialize x and y arrays on the host
-  // for (int i = 0; i < N; i++) {
-  //   x[i] = 1.0f;
-  //   y[i] = 2.0f;
-  // }
+  // Allocate 3ints per channel per pixel.
+  cudaMallocManaged(&ptr_img, img_size);
+  render<<<blocks, threads>>>(ptr_img);
 
-  // // Run kernel on 1M elements on the GPU
-  // add<<<1, 1>>>(N, x, y);
+  // Wait for GPU to finish before accessing on host
+  cudaDeviceSynchronize();
+  write_image_buffer_to_disk(ptr_img, "first.ppm");
 
-  // // Wait for GPU to finish before accessing on host
-  // cudaDeviceSynchronize();
-
-  // // Check for errors (all values should be 3.0f)
-  // float maxError = 0.0f;
-  // for (int i = 0; i < N; i++)
-  //   maxError = fmax(maxError, fabs(y[i]-3.0f));
-  // std::cout << "Max error: " << maxError << std::endl;
-
-  // // Free memory
-  // cudaFree(x);
-  // cudaFree(y);
-  
+  // Free memory
+  cudaFree(ptr_img);
   return 0;
-}
+} 
